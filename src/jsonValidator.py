a4a1cf6b2f23dc1ef8cf497efb959d57bac69946
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-02-21 12:21:50 +0000

    json validator docstr

--- src/jsonValidator.py
+++ src/jsonValidator.py
@@ -7,6 +7,17 @@
 # -------------------------------------------------------------------------- #
 
 class JsonValidator:
+    """
+    Takes input of the uploaded folder name and checks if all the JSON
+    files in the folder are in the expected and correct format.
+
+    Returns either all files are valid or returns any issues with the files.
+
+    Example returns for issues include:
+    - No json files present in the folder
+    - Unexpected JSON file name
+    - Invalid JSON structure in file X at line X - (REASON)
+    """
 
 # -------------------------------------------------------------------------- #
 
@@ -20,6 +31,11 @@ class JsonValidator:
     # ---------------------------------------------------------------------- #
 
     def getFiles(self):
+        """
+        Gets all the JSON files names given in the uploaded folder
+        :return: returns a list of the JSON files as strings
+        """
+
         pass
 
     # ---------------------------------------------------------------------- #
