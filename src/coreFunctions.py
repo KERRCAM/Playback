bdbe6ede1abd660839ac4bb46dce40d518734774
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-03-05 20:55:09 +0000

    note for search sort

--- src/coreFunctions.py
+++ src/coreFunctions.py
@@ -7,6 +7,18 @@
 # ----------------------------------------------------------------------------------------------- #
 
 class CF():
+    """
+    WHERE TO PUT SEARCH SORT ALGORITHMS
+
+    Need to prioritise good run times and efficiency here as we are dealing with
+    potentially tens of thousands of items needing sorted and hundreds of thousends
+    needing to be searched through.
+
+    So go more quick sorts, merge sorts and binary searches etc - best option in each case.
+    Things like bubble sort and linear searches will be no good.
+
+    ^ Can replace doc string once code is in, above is just a note.
+    """
 
 # ----------------------------------------------------------------------------------------------- #
 
