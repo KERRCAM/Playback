862f69f7177809d6176adea276c28fb92cbfeda4
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2025-03-19 22:10:32 +0000

    initial

--- src/jsonParser.py
+++ src/jsonParser.py
@@ -7,13 +7,16 @@
 # ----------------------------------------------------------------------------------------------- #
 
 class JsonParser(): # json object that is a list of objects maybe?
+    """
+    Takes valid JSON files and parses them in a workable python object for analysis.
+    """
 
 # ----------------------------------------------------------------------------------------------- #
 
     def __init__(self):
-        '''
+        """
 
-        '''
+        """
 
         pass
 
