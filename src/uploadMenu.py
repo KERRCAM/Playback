710ab07a0fd2b5085f84c268b19a7d8fbd4a630a
Usukhbayar Battur <usuhuubattuR@gmail.com>
2025-04-28 02:48:26 +0100

    stuff

--- src/uploadMenu.py
+++ src/uploadMenu.py
@@ -1,6 +1,6 @@
 # LIBRARY IMPORTS
 import customtkinter as ctk
-from zipfile import ZipFile # NEED TO INCLUDE IN DEPENDENCIES?
+from zipfile import ZipFile 
 from tkinter import filedialog, messagebox
 import os
 
@@ -68,7 +68,6 @@ class UploadMenu:
             else:
                 # Get the directory of the uploaded file
                 self.input_dir = os.path.dirname(self.input_path)
-                messagebox.showinfo("Success", "File successfully uploaded.")
                 extract_dir = os.path.join(self.input_dir, "extracted_files")
 
                 # Call the extraction method with the directory of the uploaded file
